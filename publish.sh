df0227cc2bb8f14cbbbab6b202c832476df31d1c
Le Ron <ronle9519@gmail.com>
2024-05-28 22:49:30 +0700

    feat: update publish profile

---
+++ publish.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+docker build -t rubeha/business_flow_customization_api_gateway -f ApiGateway/Dockerfile .
+docker build -t rubeha/business_flow_api:latest -f Services/BusinessFlow/Presentation/BusinessFlow.Api/Dockerfile .
+docker build -t rubeha/hub_api:latest -f Services/Hub/Presentation/Hub.Api/Dockerfile .
+docker build -t rubeha/identity_api:latest -f Services/Identity/Presentation/Identity.Api/Dockerfile .
+docker build -t rubeha/submission_api:latest -f Services/Submission/Presentation/Submission.Api/Dockerfile .
+
+docker push rubeha/business_flow_customization_api_gateway:latest
+docker push rubeha/business_flow_api:latest
+docker push rubeha/hub_api:latest
+docker push rubeha/identity_api:latest
+docker push rubeha/submission_api:latest
