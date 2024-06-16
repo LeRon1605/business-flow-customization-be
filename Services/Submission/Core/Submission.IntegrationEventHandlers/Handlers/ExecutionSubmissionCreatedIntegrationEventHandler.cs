379d6f741b0c81c9f9717d978908040be899aaeb
Le Ron <ronle9519@gmail.com>
2024-06-16 17:44:05 +0700

    feat: update DI

--- Services/Submission/Core/Submission.IntegrationEventHandlers/Handlers/ExecutionSubmissionCreatedIntegrationEventHandler.cs
+++ Services/Submission/Core/Submission.IntegrationEventHandlers/Handlers/ExecutionSubmissionCreatedIntegrationEventHandler.cs
@@ -21,7 +21,7 @@ public class ExecutionSubmissionCreatedIntegrationEventHandler : IntegrationEven
     private readonly ISubmissionDomainService _submissionDomainService;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IEventPublisher _eventPublisher;
-    private readonly ICommandRepository<FormSubmissionExecution> _formSubmissionExecutionRepository;
+    private readonly IRepository<FormSubmissionExecution> _formSubmissionExecutionRepository;
     
     public ExecutionSubmissionCreatedIntegrationEventHandler(ICurrentUser currentUser
         , ILogger<IntegrationEventHandler<ExecutionSubmissionCreatedIntegrationEvent>> logger
@@ -29,7 +29,7 @@ public class ExecutionSubmissionCreatedIntegrationEventHandler : IntegrationEven
         , ISubmissionDomainService submissionDomainService
         , IUnitOfWork unitOfWork
         , IEventPublisher eventPublisher
-        , ICommandRepository<FormSubmissionExecution> formSubmissionExecutionRepository) : base(currentUser, logger)
+        , IRepository<FormSubmissionExecution> formSubmissionExecutionRepository) : base(currentUser, logger)
     {
         _formRepository = formRepository;
         _submissionDomainService = submissionDomainService;
